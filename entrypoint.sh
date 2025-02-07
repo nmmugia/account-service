67424f8ac852fb96fb3b6b68ea44729e57d35e3b
Mugia N M <seemugia1@gmail.com>
2025-02-07 08:33:42 +0700

    fix:: app port assignment
    Signed-off-by: Mugia N M <seemugia1@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 
 set -e
 
+WSL_IP=$(ip route | awk '/^default via/{print $3}')
+export APP_HOST=$WSL_IP
+
 if [ "$RUN_INTEGRATION_TESTS" = "true" ]; then
   echo "Running integration tests..."
   go test -v -tags integration -coverprofile=coverage.out -coverpkg=./src/... ./test/integration
